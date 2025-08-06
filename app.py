793202c2c58f00d77635e1900f93d49366e36e98
Janarakshani <153097876+Janarakshani@users.noreply.github.com>
2025-08-06 18:00:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, request, jsonify
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 
-# Load trained model
-model = pickle.load(open('linear_model.pkl', 'rb'))
+# Load your trained model
+model = pickle.load(open('trained_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return "House Price Model API is running!"
+    return "House Price Predictor is live!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    input_features = np.array(data['input']).reshape(1, -1)
-    prediction = model.predict(input_features)
-    return jsonify({'prediction': prediction.tolist()})
+    data = request.get_json()
+    prediction = model.predict([list(data.values())])
+    return jsonify({'prediction': prediction[0]})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
